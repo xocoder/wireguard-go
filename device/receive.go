99b723cf88ea1a1246105bebc5e046997378006d
Avery Pennarun <apenwarr@gmail.com>
2019-04-20 06:12:50 -0400

    [Tailscale] Add, remove, and reduce log messages.
    Change some message prefixes.
    
    Print out the endpoint IP address when we get invalid packets.
    
    Don't bother sending initial handshake to devices with no endpoint yet,
    and log our own public key in the message, to make it easier to look up
    our key in the network map logs on the other end.
    
    Update "Sending handshake init" string to print a peer.endpoint directly.
    If the endpoint supports multiple addresses, this lets it render itself
    appropriately. And add other extra debug logs to trace the handshake
    progress.
    
    Hopefully I won't regret this. Mostly any problems we've had revolve
    around handshakes, so I'm leaving those messages enabled.

--- device/receive.go
+++ device/receive.go
@@ -94,7 +94,7 @@ func (peer *Peer) keepKeyFreshReceiving() {
  */
 func (device *Device) RoutineReceiveIncoming(IP int, bind conn.Bind) {
 
-	logDebug := device.log.Debug
+	logDebug := Silence{}
 	defer func() {
 		logDebug.Println("Routine: receive incoming IPv" + strconv.Itoa(IP) + " - stopped")
 		device.net.stopping.Done()
@@ -205,7 +205,7 @@ func (device *Device) RoutineReceiveIncoming(IP int, bind conn.Bind) {
 			okay = len(packet) == MessageCookieReplySize
 
 		default:
-			logDebug.Println("Received message with unknown type")
+			logDebug.Printf("Received message with unknown type from %v", endpoint.DstIP())
 		}
 
 		if okay {
@@ -228,7 +228,7 @@ func (device *Device) RoutineDecryption() {
 
 	var nonce [chacha20poly1305.NonceSize]byte
 
-	logDebug := device.log.Debug
+	logDebug := Silence{}
 	defer func() {
 		logDebug.Println("Routine: decryption worker - stopped")
 		device.state.stopping.Done()
@@ -301,14 +301,14 @@ func (device *Device) RoutineHandshake() {
 	var ok bool
 
 	defer func() {
-		logDebug.Println("Routine: handshake worker - stopped")
+		//logDebug.Println("Routine: handshake worker - stopped")
 		device.state.stopping.Done()
 		if elem.buffer != nil {
 			device.PutMessageBuffer(elem.buffer)
 		}
 	}()
 
-	logDebug.Println("Routine: handshake worker - started")
+	//logDebug.Println("Routine: handshake worker - started")
 	device.state.starting.Done()
 
 	for {
@@ -367,7 +367,7 @@ func (device *Device) RoutineHandshake() {
 			// check mac fields and maybe ratelimit
 
 			if !device.cookieChecker.CheckMAC1(elem.packet) {
-				logDebug.Println("Received packet with invalid mac1")
+				logDebug.Printf("Received packet with invalid mac1 from %v\n", elem.endpoint.DstIP())
 				continue
 			}
 
@@ -497,7 +497,7 @@ func (peer *Peer) RoutineSequentialReceiver() {
 	var elem *QueueInboundElement
 
 	defer func() {
-		logDebug.Println(peer, "- Routine: sequential receiver - stopped")
+		//logDebug.Println(peer, "- Routine: sequential receiver - stopped")
 		peer.routines.stopping.Done()
 		if elem != nil {
 			if !elem.IsDropped() {
@@ -507,7 +507,7 @@ func (peer *Peer) RoutineSequentialReceiver() {
 		}
 	}()
 
-	logDebug.Println(peer, "- Routine: sequential receiver - started")
+	//logDebug.Println(peer, "- Routine: sequential receiver - started")
 
 	peer.routines.starting.Done()
 
--- device/send.go
+++ device/send.go
@@ -8,6 +8,7 @@ package device
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -114,7 +115,7 @@ func (peer *Peer) SendKeepalive() bool {
 	elem.packet = nil
 	select {
 	case peer.queue.nonce <- elem:
-		peer.device.log.Debug.Println(peer, "- Sending keepalive packet")
+		//peer.device.log.Debug.Println(peer, "- Sending keepalive packet")
 		return true
 	default:
 		peer.device.PutMessageBuffer(elem.buffer)
@@ -143,7 +144,14 @@ func (peer *Peer) SendHandshakeInitiation(isRetry bool) error {
 	peer.handshake.lastSentHandshake = time.Now()
 	peer.handshake.mutex.Unlock()
 
-	peer.device.log.Debug.Println(peer, "- Sending handshake initiation")
+	peer.RLock()
+	endpoint := peer.endpoint
+	peer.RUnlock()
+	if endpoint == nil {
+		return errors.New("no peer endpoint; skipped")
+	}
+
+	peer.device.log.Debug.Printf("%v - %v Sending handshake init %v", peer, peer.device, peer.endpoint)
 
 	msg, err := peer.device.CreateMessageInitiation(peer)
 	if err != nil {
@@ -162,7 +170,7 @@ func (peer *Peer) SendHandshakeInitiation(isRetry bool) error {
 
 	err = peer.SendBuffer(packet)
 	if err != nil {
-		peer.device.log.Error.Println(peer, "- Failed to send handshake initiation", err)
+		peer.device.log.Error.Println(peer, "- Failed to send handshake initiation:", err)
 	}
 	peer.timersHandshakeInitiated()
 
@@ -245,11 +253,11 @@ func (device *Device) RoutineReadFromTUN() {
 	logError := device.log.Error
 
 	defer func() {
-		logDebug.Println("Routine: TUN reader - stopped")
+		//logDebug.Println("Routine: TUN reader - stopped")
 		device.state.stopping.Done()
 	}()
 
-	logDebug.Println("Routine: TUN reader - started")
+	//logDebug.Println("Routine: TUN reader - started")
 	device.state.starting.Done()
 
 	var elem *QueueOutboundElement
@@ -353,13 +361,13 @@ func (peer *Peer) RoutineNonce() {
 
 	defer func() {
 		flush()
-		logDebug.Println(peer, "- Routine: nonce worker - stopped")
+		//logDebug.Println(peer, "- Routine: nonce worker - stopped")
 		peer.queue.packetInNonceQueueIsAwaitingKey.Set(false)
 		peer.routines.stopping.Done()
 	}()
 
 	peer.routines.starting.Done()
-	logDebug.Println(peer, "- Routine: nonce worker - started")
+	//logDebug.Println(peer, "- Routine: nonce worker - started")
 
 	for {
 	NextPacket:
@@ -404,7 +412,7 @@ func (peer *Peer) RoutineNonce() {
 
 				// wait for key to be established
 
-				logDebug.Println(peer, "- Awaiting keypair")
+				//logDebug.Println(peer, "- Awaiting keypair")
 
 				select {
 				case <-peer.signals.newKeypairArrived:
@@ -476,7 +484,7 @@ func (device *Device) RoutineEncryption() {
 
 	var nonce [chacha20poly1305.NonceSize]byte
 
-	logDebug := device.log.Debug
+	//logDebug := device.log.Debug
 
 	defer func() {
 		for {
@@ -492,11 +500,11 @@ func (device *Device) RoutineEncryption() {
 			}
 		}
 	out:
-		logDebug.Println("Routine: encryption worker - stopped")
+		//logDebug.Println("Routine: encryption worker - stopped")
 		device.state.stopping.Done()
 	}()
 
-	logDebug.Println("Routine: encryption worker - started")
+	//logDebug.Println("Routine: encryption worker - started")
 	device.state.starting.Done()
 
 	for {
@@ -561,7 +569,7 @@ func (peer *Peer) RoutineSequentialSender() {
 
 	device := peer.device
 
-	logDebug := device.log.Debug
+	//logDebug := device.log.Debug
 	logError := device.log.Error
 
 	defer func() {
@@ -580,11 +588,11 @@ func (peer *Peer) RoutineSequentialSender() {
 			}
 		}
 	out:
-		logDebug.Println(peer, "- Routine: sequential sender - stopped")
+		//logDebug.Println(peer, "- Routine: sequential sender - stopped")
 		peer.routines.stopping.Done()
 	}()
 
-	logDebug.Println(peer, "- Routine: sequential sender - started")
+	//logDebug.Println(peer, "- Routine: sequential sender - started")
 
 	peer.routines.starting.Done()
 
