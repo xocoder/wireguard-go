97b4c082755f7538f02b98a2db5906bf05d64514
Avery Pennarun <apenwarr@gmail.com>
2020-01-30 07:24:31 -0500

    device: consistently use atomic to get/set keypairs.current.sendNonce
    We were reading it atomically, but not setting it atomically.
    
    Signed-off-by: Avery Pennarun <apenwarr@gmail.com>

--- device/peer.go
+++ device/peer.go
@@ -251,7 +251,7 @@ func (peer *Peer) ExpireCurrentKeypairs() {
 	keypairs := &peer.keypairs
 	keypairs.Lock()
 	if keypairs.current != nil {
-		keypairs.current.sendNonce = RejectAfterMessages
+		atomic.StoreUint64(&keypairs.current.sendNonce, RejectAfterMessages)
 	}
 	if keypairs.next != nil {
 		keypairs.loadNext().sendNonce = RejectAfterMessages
