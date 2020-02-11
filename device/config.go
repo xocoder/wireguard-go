8319dd8f16c03246678574f03bd7b0892aa24408
David Crawshaw <david@zentus.com>
2020-02-10 20:57:49 -0500

    device: direct API for configuration

---
+++ device/config.go
@@ -0,0 +1,179 @@
+// SPDX-License-Identifier: MIT
+
+package device
+
+import (
+	"fmt"
+	"sort"
+	"sync/atomic"
+
+	"github.com/tailscale/wireguard-go/ipc"
+	"github.com/tailscale/wireguard-go/wgcfg"
+)
+
+func (device *Device) Config() *wgcfg.Config {
+	device.net.RLock()
+	listenPort := device.net.port
+	device.net.RUnlock()
+
+	device.staticIdentity.RLock()
+	privateKey := device.staticIdentity.privateKey
+	device.staticIdentity.RUnlock()
+
+	device.peers.RLock()
+	keyMap := device.peers.keyMap
+	device.peers.RUnlock()
+
+	cfg := &wgcfg.Config{
+		PrivateKey: privateKey,
+		ListenPort: listenPort,
+	}
+	for _, peer := range keyMap {
+		peer.RLock()
+		p := wgcfg.Peer{
+			PublicKey:           peer.handshake.remoteStatic,
+			PresharedKey:        peer.handshake.presharedKey,
+			PersistentKeepalive: uint16(atomic.LoadUint32(&peer.persistentKeepaliveInterval)),
+		}
+		if peer.endpoint != nil {
+			p.Endpoints = peer.endpoint.Addrs()
+		}
+		for _, ipnet := range device.allowedips.EntriesForPeer(peer) {
+			ones, _ := ipnet.Mask.Size()
+			cidr := wgcfg.CIDR{
+				Mask: uint8(ones),
+			}
+			copy(cidr.IP.Addr[:], ipnet.IP.To16())
+			p.AllowedIPs = append(p.AllowedIPs, cidr)
+		}
+		peer.RUnlock()
+
+		cfg.Peers = append(cfg.Peers, p)
+	}
+	sort.Slice(cfg.Peers, func(i, j int) bool {
+		return cfg.Peers[i].PublicKey.LessThan(&cfg.Peers[j].PublicKey)
+	})
+
+	return cfg
+}
+
+// Reconfig replaces the existing device configuration with cfg.
+func (device *Device) Reconfig(cfg *wgcfg.Config) (err error) {
+	defer func() {
+		if err != nil {
+			device.log.Debug.Printf("device.Reconfig: failed: %v", err)
+			device.RemoveAllPeers()
+		}
+	}()
+
+	// Remove any currentt peers not in the new configuration.
+	device.peers.RLock()
+	oldPeers := make(map[wgcfg.Key]bool)
+	for k := range device.peers.keyMap {
+		oldPeers[k] = true
+	}
+	device.peers.RUnlock()
+	for _, p := range cfg.Peers {
+		delete(oldPeers, p.PublicKey)
+	}
+	for k := range oldPeers {
+		device.log.Debug.Printf("device.Reconfig: removing old peer %s", k.ShortString())
+		device.RemovePeer(k)
+	}
+
+	device.staticIdentity.Lock()
+	curPrivKey := device.staticIdentity.privateKey
+	device.staticIdentity.Unlock()
+
+	if !curPrivKey.Equal(cfg.PrivateKey) {
+		device.log.Debug.Println("device.Reconfig: resetting private key")
+		if err := device.SetPrivateKey(cfg.PrivateKey); err != nil {
+			return err
+		}
+	}
+
+	device.net.Lock()
+	device.net.port = cfg.ListenPort
+	device.net.Unlock()
+
+	if err := device.BindUpdate(); err != nil {
+		return ErrPortInUse
+	}
+
+	// TODO(crawshaw): UAPI supports an fwmark field
+
+	newKeepalivePeers := make(map[wgcfg.Key]*Peer)
+	for _, p := range cfg.Peers {
+		peer := device.LookupPeer(p.PublicKey)
+		if peer == nil {
+			device.log.Debug.Printf("device.Reconfig: new peer %s", p.PublicKey.ShortString())
+			peer, err = device.NewPeer(p.PublicKey)
+			if err != nil {
+				return err
+			}
+			if p.PersistentKeepalive != 0 && device.isUp.Get() {
+				newKeepalivePeers[p.PublicKey] = peer
+			}
+		}
+
+		if !p.PresharedKey.IsZero() {
+			peer.handshake.mutex.Lock()
+			peer.handshake.presharedKey = p.PresharedKey
+			peer.handshake.mutex.Unlock()
+
+			device.log.Debug.Printf("device.Reconfig: setting preshared key for peer %s", p.PublicKey.ShortString())
+		}
+
+		peer.Lock()
+		atomic.StoreUint32(&peer.persistentKeepaliveInterval, uint32(p.PersistentKeepalive))
+		if len(p.Endpoints) > 0 && (peer.endpoint == nil || !endpointsEqual(p.Endpoints, peer.endpoint.Addrs())) {
+			str := p.Endpoints[0].String()
+			for _, cfgEp := range p.Endpoints[1:] {
+				str += "," + cfgEp.String()
+			}
+			ep, err := device.createEndpoint(p.PublicKey, str)
+			if err != nil {
+				peer.Unlock()
+				return err
+			}
+			peer.endpoint = ep
+		}
+		peer.Unlock()
+
+		device.allowedips.RemoveByPeer(peer)
+		for _, allowedIP := range p.AllowedIPs {
+			ones := uint(allowedIP.Mask)
+			ip := allowedIP.IP.IP()
+			if allowedIP.IP.Is4() {
+				ip = ip.To4()
+			}
+			device.allowedips.Insert(ip, ones, peer)
+		}
+	}
+
+	// Send immediate keepalive if we're turning it on and before it wasn't on.
+	for k, peer := range newKeepalivePeers {
+		device.log.Debug.Printf("device.Reconfig: sending keepalive to peer %s", k.ShortString())
+		peer.SendKeepalive()
+	}
+
+	return nil
+}
+
+func endpointsEqual(x, y []wgcfg.Endpoint) bool {
+	if len(x) != len(y) {
+		return false
+	}
+	eps := make(map[wgcfg.Endpoint]bool)
+	for _, ep := range x {
+		eps[ep] = true
+	}
+	for _, ep := range y {
+		if !eps[ep] {
+			return false
+		}
+	}
+	return true
+}
+
+var ErrPortInUse = fmt.Errorf("wireguard: local port in use: %w", &IPCError{ipc.IpcErrorPortInUse})
