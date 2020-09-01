87212a9e7a6011980e24db488b28a0e2515ab53d
Josh Bleecher Snyder <josharian@gmail.com>
2020-08-31 17:24:05 -0700

    device: return errors from Peer.Start
    And propagate them.

--- device/peer.go
+++ device/peer.go
@@ -169,12 +169,11 @@ func (peer *Peer) String() string {
 	return peer.handshake.remoteStatic.ShortString()
 }
 
-func (peer *Peer) Start() {
+func (peer *Peer) Start() error {
 
 	// should never start a peer on a closed device
-
 	if peer.device.isClosed.Get() {
-		return
+		return errors.New("Start called on closed device")
 	}
 
 	// prevent simultaneous start/stop operations
@@ -183,7 +182,7 @@ func (peer *Peer) Start() {
 	defer peer.routines.Unlock()
 
 	if peer.isRunning.Get() {
-		return
+		return errors.New("Start called on running device")
 	}
 
 	device := peer.device
@@ -220,6 +219,7 @@ func (peer *Peer) Start() {
 
 	peer.routines.starting.Wait()
 	peer.isRunning.Set(true)
+	return nil
 }
 
 func (peer *Peer) ZeroAndFlushAll() {
