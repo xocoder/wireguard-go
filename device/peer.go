db78fad0bebff6d168eafe45eee6e5ed04230c97
David Anderson <danderson@tailscale.com>
2020-12-19 17:10:20 -0800

    device: don't log per-packet in a hot code path.
    This is a temporary fix for our UpdateDst patch. The medium-term
    plan is to remove that patch entirely and revert to upstream, but
    to make default routing work without logspam right now, just shut
    up the logspam in question.
    
    Signed-off-by: David Anderson <danderson@tailscale.com>

--- device/peer.go
+++ device/peer.go
@@ -305,7 +305,6 @@ func (peer *Peer) SetEndpointAddress(addr *net.UDPAddr) {
 		return
 	}
 	if p := peer.device.allowedips.LookupIP(addr.IP); p != nil {
-		peer.device.log.Debug.Printf("%v - SetEndPointAddress: %v owned by %v, skipping", peer, addr, p)
 		return
 	}
 
